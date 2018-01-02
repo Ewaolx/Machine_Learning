3cddce2a0c0550369e15e75d00640f07e1098075
Ewaolx <34932185+Ewaolx@users.noreply.github.com>
2018-01-02 09:29:47 +0530

    Update main.py

--- hand_written_digit_classification/main.py
+++ hand_written_digit_classification/main.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 13 14:14:29 2017
 
-@author: Vicky
-"""
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
